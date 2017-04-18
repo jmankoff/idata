2dc0c9a2c9971f052aa196cdcb431894fbf1eb1c
Jen <jmankoff@cs.cmu.edu>
2017-04-18 02:17:58 +0000

    moving dev to lfs

--- install_scripts/git_lfs.sh
+++ install_scripts/git_lfs.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-
 #git lfs
-#sudo apt-get install python-software-properties
-#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-#sudo apt-get install git-lfs
-#git lfs install
-#cd ~/idata
-#git lfs track "*.gz"
-#git add .gitattributes
+sudo apt-get install python-software-properties
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs install
+cd ~/idata
+git lfs track "*.zip"
+git lfs track "*.gz"
+git add .gitattributes
